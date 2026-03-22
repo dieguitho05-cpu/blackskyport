66c1302fb0784abb0670b70864ddbde06dfaaf00
dieguitho05-cpu <dieguitho05@gmail.com>
2026-03-22 14:47:39 -0500

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // ======================
-// VARIABLES
+// DATOS
 // ======================
 let datos = JSON.parse(localStorage.getItem("datos")) || [];
 
 // ======================
-// FUNCIONES
+// NAVEGACIÓN
 // ======================
-
 function mostrar(vista) {
   document.querySelectorAll(".vista").forEach(v => v.style.display = "none");
   document.getElementById(vista).style.display = "block";
 }
 
+// ======================
+// GUARDAR
+// ======================
 function guardar() {
   const cliente = document.getElementById("cliente").value;
   const origen = document.getElementById("origen").value;
@@ -27,17 +29,60 @@ function guardar() {
   mostrarLista();
 }
 
+// ======================
+// MOSTRAR LISTA
+// ======================
 function mostrarLista() {
   const contenedor = document.getElementById("lista");
   contenedor.innerHTML = "";
 
-  datos.forEach((d, i) => {
+  datos.forEach(d => {
     contenedor.innerHTML += `
       <div>
         ${d.cliente} - ${d.origen} → ${d.destino} - S/ ${d.precio}
       </div>
     `;
   });
+}
+
+// ======================
+// EXPORTAR EXCEL
+// ======================
+function exportarExcel() {
+  const hoja = XLSX.utils.json_to_sheet(datos);
+
+  const libro = XLSX.utils.book_new();
+
+  XLSX.utils.book_append_sheet(libro, hoja, "Datos");
+
+  XLSX.writeFile(libro, "blackskyport.xlsx");
+}
+
+// ======================
+// IMPORTAR EXCEL
+// ======================
+function importarExcel(event) {
+  const archivo = event.target.files[0];
+
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    const data = new Uint8Array(e.target.result);
+
+    const workbook = XLSX.read(data, { type: "array" });
+
+    const hoja = workbook.Sheets[workbook.SheetNames[0]];
+
+    const json = XLSX.utils.sheet_to_json(hoja);
+
+    datos = json;
+
+    localStorage.setItem("datos", JSON.stringify(datos));
+
+    mostrarLista();
+  };
+
+  reader.readAsArrayBuffer(archivo);
 }
 
 // ======================
